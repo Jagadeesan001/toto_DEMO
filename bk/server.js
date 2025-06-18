1829d0d719a7b845dfa65e15ed40cd26e1b415d2
Jagadeesan001 <jagadeesanmass001@gmail.com>
2025-06-18 07:45:56 +0530

    link mongodb atles

--- bk/server.js
+++ bk/server.js
@@ -1,35 +1,45 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
-const cors = require("cors")
-app.use(cors())
-app.use(express.json())
-require("dotenv").config()
-mongoose.connect("mongodb://localhost:27017/clg").then(() => { console.log("Db connected success"); })
-    .catch(() => console.log("Db connection Error"))
-const stdlist = mongoose.model("stdlist", {
-    name: String
-}, "std")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+mongoose
+  .connect("mongodb+srv://jagadeesan:8248095993@jagadeesan.xfy1xyw.mongodb.net/clg?retryWrites=true&w=majority&appName=jagadeesan")
+  .then(() => {
+    console.log("Db connected success");
+  })
+  .catch(() => console.log("Db connection Error"));
+const stdlist = mongoose.model(
+  "stdlist",
+  {
+    name: String,
+  },
+  "std"
+);
 app.listen(process.env.PORT, () => {
-    console.log("server srarted");
-})
+  console.log("server srarted");
+});
 app.get("/data", (req, res) => {
-    stdlist.find().then((data) => {
-        res.send(data)
-    })
-})
+  stdlist.find().then((data) => {
+    res.send(data);
+  });
+});
 app.post("/datasent", (req, res) => {
-    var ret = req.body.newvalue
-    if(ret.length>0){
-        var name = new stdlist({ name: ret })
-        name.save().then(() => console.log("db added..........."))
-    }
-})
-app.post("/delete", (req, res) => {
-    var dbdel = req.body.delval
-    // console.log(dbdel);
-    stdlist.deleteOne({ name: dbdel }).then(()=>{console.log("db Deleted...")})
-    .catch((err)=>console.log(err))
-})
-
-
+  var ret = req.body.newvalue;
+  if (ret.length > 0) {
+    var name = new stdlist({ name: ret });
+    name.save().then(() => console.log("db added..........."));
+  }
+});
+app.delete("/delete", (req, res) => {
+  var dbdel = req.body.delval;
+  // console.log(dbdel);
+  stdlist
+    .deleteOne({ name: dbdel })
+    .then(() => {
+      console.log("db Deleted...");
+    })
+    .catch((err) => console.log(err));
+});
--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,47 +1,69 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-  const [value, setvalue] = useState("")
-  const [arr, setarr] = useState([])
+  const [value, setvalue] = useState("");
+  const [arr, setarr] = useState([]);
   function handlevalue(evt) {
-    setvalue(evt.target.value)
+    setvalue(evt.target.value);
   }
   const addvluetoarr = () => {
-    axios.post("http://localhost:4000/datasent", { newvalue: value })
-    setvalue("")
-  }
+    axios.post("http://localhost:4000/datasent", { newvalue: value });
+    setvalue("");
+  };
   useEffect(() => {
     axios.get("http://localhost:4000/data").then((data) => {
-      setarr(data.data)
-      // console.log(data.data);
-    }, [])
-  })
+      setarr(data.data);
+
+      // console.log(data.data)
+    }, []);
+  });
   function deletedbvalue(props) {
-    var dbindex = props.itm.itm.name
+    var dbindex = props.itm.itm.name;
     // console.log(dbindex);
-    axios.post("http://localhost:4000/delete", { delval: dbindex })
+    axios.delete("http://localhost:4000/delete", { delval: dbindex });
   }
   return (
     <>
-    <div className="py-8">
-      <div className="flex gap-4">
-        <input value={value} onChange={handlevalue} type="text" className="inputBox"/>
-        <button onClick={addvluetoarr} className="text-white w-16 bg-black rounded">Add..</button>
-      </div>
+      <div className="py-8">
+        <div className="flex gap-4">
+          <input
+            value={value}
+            onChange={handlevalue}
+            type="text"
+            className="inputBox"
+          />
+          <button
+            onClick={addvluetoarr}
+            className="text-white w-16 bg-black rounded"
+          >
+            Add..
+          </button>
+        </div>
 
-      <ul  className="text-white">
-        {
-          arr.map((itm, index) => {
+        <ul className="text-white">
+          {arr.map((itm, index) => {
             return (
-              <div key={index} style={{ display: "flex" }} className="flex justify-between bold item_show_container">
-                <h1 >{itm.name}</h1><button onClick={() => { deletedbvalue({ index: { index }, itm: { itm } }) }} style={{ height: "fit-content" }} className="delete_BTN">Delete</button>
+              <div
+                key={index}
+                style={{ display: "flex" }}
+                className="flex justify-between bold item_show_container"
+              >
+                <h1>{itm.name}</h1>
+                <button
+                  onClick={() => {
+                    deletedbvalue({ index: { index }, itm: { itm } });
+                  }}
+                  style={{ height: "fit-content" }}
+                  className="delete_BTN"
+                >
+                  Delete
+                </button>
               </div>
-            )
-          })
-        }
-      </ul>
-    </div>
+            );
+          })}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
